b22d36d1f9dad0f4b8d670ff05dd9711b2c0f84f
Austin Moran <a.moran3257@gmail.com>
2018-12-13 10:22:54 -0700

    reduce warmup complete

--- reduce_intro_warmup/main.js
+++ reduce_intro_warmup/main.js
@@ -1,21 +1,36 @@
 
 //add all elements together
 function sum(arr){
-
+  return arr.reduce((acc, cur) => {
+    return acc + cur
+  }, 0)
 }
 
 // multiply all elements together
 function multiply(arr){
-
+  return arr.reduce((acc, cur) => {
+    return acc * cur
+  }, 1)
 }
 
 //double each element using reduce
 function double(arr){
-
+  return arr.reduce((prev, cur) => {
+    prev.push(cur * 2)
+    return prev
+  }, [])
 }
 
 // //count the letters
 function countLetters(str){
+  let string = []
+  for (let i=0; i<str.length; i++){
+    string[i] = str[i]
+  }
+  return string.reduce((prev, cur) => {
+    prev[cur] = (prev[cur] || 0) + 1
+    return prev
+  }, {})
 
 }
 
